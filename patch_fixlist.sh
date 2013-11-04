82be89a5ca0a42057b36793ea5e2555497ae234c
Kent Fredric <kentfredric@gmail.com>
2013-11-05 00:41:16 +1300

    Add fix data

---
+++ patch_fixlist.sh
@@ -0,0 +1,7 @@
+if [ -z $1 ]; then
+  echo "Please provide perlbrew root path as \$1"
+  exit 1;
+fi
+for i in $( cat ./fixlist.txt ); do
+  sed -i "s^/home/kent/perl5/perlbrew/perls/5.8.9-pristine^$1^g" $i;
+done
